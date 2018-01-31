ec12b19ad77e5310a7d49df6e5b06eff917d9c4c
nadineducegep <nadineducegep@gmail.com>
2018-01-31 10:02:13 -0500

    Initialisation Gyro et son PID

--- src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
+++ src/org/usfirst/frc/equipe5910/robot/RobotControleur.java
@@ -1,6 +1,11 @@
 package org.usfirst.frc.equipe5910.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSourceType;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -10,7 +15,56 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  * directory.
  */
 public class RobotControleur extends IterativeRobot {
+	
+	public static final double DISTANCE_KP = 0.18; //0.11;
+	public static final double DISTANCE_KI = 0.00045; //0.00045;
+	public static final float DISTANCE_TOLERANCE = 0.083f;
 
+	public class SortiePID implements PIDOutput {
+
+		double distanceSortiePID;
+		
+		@Override
+		public void pidWrite(double sortie) {
+			distanceSortiePID = sortie;
+		}
+		
+		public double getPIDOut() {
+			return distanceSortiePID;
+		}
+	}		
+	
+	public static final double GYRO_KP = 0.02; //0.03
+	public static final double GYRO_KI = 0.0;
+	
+	public static final double GYRO_KP_ROTATION = 0.00075; // + haut = plus aggressif
+	public static final double GYRO_KI_ROTATION = 0.000085; // + bas = plus aggressif
+	public static final boolean GYRO_INVERSE = true;
+	
+	public static final int BOUTON_GYRO_RESET = 1;
+	
+	// PWM Outputs
+	public static final int ROUE_AVANT_GAUCHE = 10; // SP 1
+	public static final int ROUE_ARRIERE_GAUCHE = 14; // SP 2
+	public static final int ROUE_AVANT_DROIT= 13; // SP 3
+	public static final int ROUE_ARRIERE_DROIT = 17; // SP 4
+	
+	public static final boolean INVERSION_ROUE_AVANT_GAUCHE = true;
+	public static final boolean INVERSION_ROUE_ARRIERE_GAUCHE = true;
+	public static final boolean INVERSION_ROUE_AVANT_DROIT = false;
+	public static final boolean INVERSION_ROUE_ARRIERE_DROIT= false;	
+	
+	GyroADXRS450 gyro;
+	PIDController pidGyro;
+	SortiePID gyroSortiePID;
+	
+    VictorSP roueAvantGauche; 
+	VictorSP roueArriereGauche; 
+	VictorSP roueAvantDroite; 
+	VictorSP roueArriereDroite; 
+	
+	
+	
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
@@ -19,6 +73,25 @@ public class RobotControleur extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		System.out.println("robotInit()");
+		 
+		roueAvantGauche = new VictorSP(ROUE_AVANT_GAUCHE);
+		roueArriereGauche = new VictorSP(ROUE_ARRIERE_GAUCHE);
+		roueAvantDroite = new VictorSP(ROUE_AVANT_DROIT);
+		roueArriereDroite = new VictorSP(ROUE_ARRIERE_DROIT);
+		 	
+		roueAvantGauche.setInverted(INVERSION_ROUE_AVANT_GAUCHE); // TRUE
+		roueArriereGauche.setInverted(INVERSION_ROUE_ARRIERE_GAUCHE); // TRUE
+		roueAvantDroite.setInverted(INVERSION_ROUE_AVANT_DROIT);
+		roueArriereDroite.setInverted(INVERSION_ROUE_ARRIERE_DROIT);
+		
+		 gyro = new GyroADXRS450();
+		 gyro.setPIDSourceType(PIDSourceType.kDisplacement);
+		 gyroSortiePID = new SortiePID();
+		 
+		 pidGyro = new PIDController(-GYRO_KP, GYRO_KI_ROTATION, 0, gyro, gyroSortiePID);
+		 pidGyro.setSetpoint(0.0f);
+		 pidGyro.setAbsoluteTolerance(3);
+		 pidGyro.enable();
 	}
 
 	/**
